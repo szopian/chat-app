46bf9065480aa077e529d35ea58c4908799b6dde
Adri Ana <szopiangreg@gmail.com>
2021-04-07 22:19:15 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       projectID="5a7581d9-1fe1-4be2-8ec2-fecda1814be9"
       userName="Greg"
       userSecret="qwerty"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 }
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,6 @@
+const ChatFeed = (props) => {
+  console.log(props);
+  return <div>ChatFeed</div>;
+};
+
+export default ChatFeed;
