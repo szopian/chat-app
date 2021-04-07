1f221db511877f20f47bf34b81ac43a031662857
Adri Ana <szopiangreg@gmail.com>
2021-04-07 14:42:35 +0100

    add chatengine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { ChatEngine } from "react-chat-engine";
 
 function App() {
-  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="5a7581d9-1fe1-4be2-8ec2-fecda1814be9"
+      userName="Greg"
+      userSecret="qwerty"
+    />
+  );
 }
 
 export default App;
