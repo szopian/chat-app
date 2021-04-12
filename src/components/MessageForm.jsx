8bade8876eff1646551fdeae73f8c5431a853f14
Adri Ana <szopiangreg@gmail.com>
2021-04-12 16:34:18 +0100

    work

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const MessageFrom = () => {
-  return <div>MessageForm</div>;
+  const [value, setValue] = useState("");
+
+  const handleChange () => {}
+
+  const handleSubmit () => {}
+
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </form>
+  );
 };
 
 export default MessageFrom;
